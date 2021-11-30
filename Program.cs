7bd97380c1a563e4ee6d04be8aba3201fd72371e
playerplayer <bohdan.k2000@gmail.com>
2021-11-30 00:53:34 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoNamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
